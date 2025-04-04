264c96fe5292bc7f6f3838db63aaafa09653e8c3
palpasa4 <palpasa04@gmail.com>
2025-04-04 13:07:55 +0545

    rye set up and implementation and used 'black' for code formatting

--- src/core/auth/auth_bearer.py
+++ src/core/auth/auth_bearer.py
@@ -1,23 +1,30 @@
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt
-from .auth_handler import decode_jwt, JWT_ALGORITHM,JWT_SECRET
+from .auth_handler import decode_jwt, JWT_ALGORITHM, JWT_SECRET
 from typing import Optional
+
 
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: Optional[HTTPAuthorizationCredentials] = await super(JWTBearer, self).__call__(request)
+        credentials: Optional[HTTPAuthorizationCredentials] = await super(
+            JWTBearer, self
+        ).__call__(request)
         if credentials:
             if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+                raise HTTPException(
+                    status_code=403, detail="Invalid authentication scheme."
+                )
             if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
-            token=credentials.credentials
+                raise HTTPException(
+                    status_code=403, detail="Invalid token or expired token."
+                )
+            token = credentials.credentials
             decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-            username = decoded_token.get("user_id")  
+            username = decoded_token.get("user_id")
             return username
         else:
             raise HTTPException(status_code=403, detail="Invalid authorization code.")
--- src/core/handlers/request_handlers/response_handler.py
+++ src/core/handlers/request_handlers/response_handler.py
@@ -1,7 +1,5 @@
 from fastapi.responses import JSONResponse
 
+
 def json_response(content, status_code: int):
-    return JSONResponse(
-        content=content,
-        status_code=status_code
-    )
+    return JSONResponse(content=content, status_code=status_code)
